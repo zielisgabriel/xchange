6ea453ffdbdc28937a92d6e78c91c93d9defc051
zielisgabriel <josegabrielas0586@email.com>
2026-03-23 20:52:22 -0300

    feat(exceptions): add BirthDateException for handling birth date validation errors

---
+++ backend/xchange_api/src/main/java/br/com/xchange/api/domain/exceptions/BirthDateException.java
@@ -0,0 +1,7 @@
+package br.com.xchange.api.domain.exceptions;
+
+public class BirthDateException extends RuntimeException {
+  public BirthDateException(String message) {
+    super(message);
+  }
+}
