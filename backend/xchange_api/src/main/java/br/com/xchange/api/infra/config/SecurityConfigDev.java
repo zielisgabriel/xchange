22cc2feaae213534535d0db5664e2a51df877e4c
zielisgabriel <josegabrielas0586@email.com>
2026-03-25 18:06:47 -0300

    feat(auth): add Argon2PasswordEncoder bean for password encoding

--- backend/xchange_api/src/main/java/br/com/xchange/api/infra/config/SecurityConfigDev.java
+++ backend/xchange_api/src/main/java/br/com/xchange/api/infra/config/SecurityConfigDev.java
@@ -7,6 +7,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -44,5 +46,10 @@ public class SecurityConfigDev {
     filter.setFilterProcessesUrl("/auth/login");
 
     return filter;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
   }
 }
