c443040b915296e01b80eed37033124151747ecb
zielisgabriel <josegabrielas0586@email.com>
2026-03-25 23:37:16 -0300

    fix(exceptions): update response status to CONFLICT for UserAlreadyExistsException

--- backend/xchange_api/src/main/java/br/com/xchange/api/domain/exceptions/UserAlreadyExistsException.java
+++ backend/xchange_api/src/main/java/br/com/xchange/api/domain/exceptions/UserAlreadyExistsException.java
@@ -3,7 +3,7 @@ package br.com.xchange.api.domain.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class UserAlreadyExistsException extends RuntimeException {
   public UserAlreadyExistsException(String message) {
     super(message);
