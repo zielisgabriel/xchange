07c8f2daddf5aaa7bae33bf7ad2b9d91d31e56c9
zielisgabriel <josegabrielas0586@email.com>
2026-03-25 23:29:51 -0300

    feat(auth): add XchangeUserDetailsService for user details management

---
+++ backend/xchange_api/src/main/java/br/com/xchange/api/infra/services/XchangeUserDetailsService.java
@@ -0,0 +1,28 @@
+package br.com.xchange.api.infra.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.xchange.api.domain.entities.AuthUser;
+import br.com.xchange.api.domain.ports.repositories.AuthUserRepositoryPort;
+import br.com.xchange.api.infra.entities.AuthUserJpa;
+import br.com.xchange.api.infra.entities.XchangeUserDetails;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class XchangeUserDetailsService implements UserDetailsService {
+  private final AuthUserRepositoryPort repositoryPort;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    AuthUser authUser = this.repositoryPort.findByEmail(email)
+      .orElseThrow(() -> new UsernameNotFoundException("Email não encontrado!"));
+
+    UserDetails userDetails = new XchangeUserDetails(AuthUserJpa.fromDomain(authUser));
+
+    return userDetails;
+  }
+}
