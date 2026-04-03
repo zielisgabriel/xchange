6b88e71e6639eff70cc71d98028ac10770a0a6f7
zielisgabriel <josegabrielas0586@email.com>
2026-04-03 18:50:20 -0300

    feat: add initial home screen component to tabs directory

---
+++ app/xchange_app/src/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@/components/ui/text";
+import { View } from "react-native";
+
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-foreground">
+        Home screen
+      </Text>
+    </View>
+  )
+}
