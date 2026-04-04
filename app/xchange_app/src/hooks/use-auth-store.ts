0c1e3610351685f3f50a8ad86a89bf29361e7c65
zielisgabriel <josegabrielas0586@email.com>
2026-04-04 16:28:00 -0300

    feat: implement persistent authentication store using zustand and secure storage

---
+++ app/xchange_app/src/hooks/use-auth-store.ts
@@ -0,0 +1,54 @@
+import { create } from "zustand"
+import { persist, createJSONStorage } from "expo-zustand-persist"
+import { getItemAsync, setItemAsync, deleteItemAsync } from "expo-secure-store"
+
+type UserState = {
+  isAuthenticated: boolean;
+  logIn: () => void;
+  logOut: () => void;
+}
+
+import { Platform } from "react-native"
+
+const authStorage = {
+  getItem: async (key: string) => {
+    if (Platform.OS === "web") return localStorage.getItem(key);
+    return await getItemAsync(key);
+  },
+  setItem: async (key: string, value: string) => {
+    if (Platform.OS === "web") return localStorage.setItem(key, value);
+    return await setItemAsync(key, value);
+  },
+  removeItem: async (key: string) => {
+    if (Platform.OS === "web") return localStorage.removeItem(key);
+    return await deleteItemAsync(key);
+  }
+}
+
+export const useAuthStore = create<UserState>(
+  persist(
+    (set) => ({
+      isAuthenticated: false,
+      logIn: () => {
+        set((state) => {
+          return {
+            ...state,
+            isAuthenticated: true
+          }
+        })
+      },
+      logOut: () => {
+        set((state) => {
+          return {
+            ...state,
+            isAuthenticated: false
+          }
+        })
+      }
+    }),
+    {
+      name: "auth-store",
+      storage: createJSONStorage(() => authStorage)
+    }
+  )
+)
