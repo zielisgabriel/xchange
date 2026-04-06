2a86f7380d978e449fd82fe53a970709dabc4c21
zielisgabriel <josegabrielas0586@gmail.com>
2026-04-06 20:00:41 -0300

    feat: add reusable Input component and server-side fetch client utility

--- app/xchange_app/src/components/ui/input.tsx
+++ app/xchange_app/src/components/ui/input.tsx
@@ -11,10 +11,10 @@ function Input({
       className={cn(
         'dark:bg-input/30 border-input bg-background text-foreground flex h-10 w-full min-w-0 flex-row items-center rounded-md border px-3 py-1 text-base leading-5 shadow-sm shadow-black/5 sm:h-9',
         props.editable === false &&
-          cn(
-            'opacity-50',
-            Platform.select({ web: 'disabled:pointer-events-none disabled:cursor-not-allowed' })
-          ),
+        cn(
+          'opacity-50',
+          Platform.select({ web: 'disabled:pointer-events-none disabled:cursor-not-allowed' })
+        ),
         Platform.select({
           web: cn(
             'placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground outline-none transition-[color,box-shadow] md:text-sm',
