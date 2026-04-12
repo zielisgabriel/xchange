59a6f68389a4ebf8d75c1e9b68d84b3b162d77d7
zielisgabriel <josegabrielas0586@gmail.com>
2026-04-12 18:53:04 -0300

    feat: implement profile management infrastructure with security configuration and global exception handling

--- backend/xchange_api/src/main/java/br/com/xchange/api/domain/exceptions/UserNotFoundException.java
+++ backend/xchange_api/src/main/java/br/com/xchange/api/domain/exceptions/UserNotFoundException.java
@@ -8,4 +8,8 @@ public class UserNotFoundException extends RuntimeException {
   public UserNotFoundException() {
     super("Usuário não encontrado.");
   }
+
+  public UserNotFoundException(String message) {
+    super(message);
+  }
 }
