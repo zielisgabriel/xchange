67a7abfd1f9f71ea37a65ad06197c9877de1a797
zielisgabriel <josegabrielas0586@gmail.com>
2026-04-12 18:52:46 -0300

    feat: implement profile management module with domain entities, JPA persistence, and use cases

--- backend/xchange_api/src/main/java/br/com/xchange/api/application/usecase/GetProfileUseCase.java
+++ backend/xchange_api/src/main/java/br/com/xchange/api/application/usecase/GetProfileUseCase.java
@@ -4,18 +4,18 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
-import br.com.xchange.api.domain.entities.AuthUser;
+import br.com.xchange.api.domain.entities.Profile;
 import br.com.xchange.api.domain.exceptions.UserNotFoundException;
-import br.com.xchange.api.domain.ports.repositories.AuthUserRepositoryPort;
+import br.com.xchange.api.domain.ports.repositories.ProfileRepositoryPort;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class GetProfileUseCase {
-  private final AuthUserRepositoryPort repositoryPort;
+  private final ProfileRepositoryPort repositoryPort;
 
-  public AuthUser execute(UUID userId) {
+  public Profile execute(UUID userId) {
     return this.repositoryPort.findById(userId)
-      .orElseThrow(() -> new UserNotFoundException());
+      .orElseThrow(() -> new UserNotFoundException("Perfil não encontrado ou não existe."));
   }
 }
