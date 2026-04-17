3160f745cf365dd6b64c50ebc430672715136d96
zielisgabriel <josegabrielas0586@gmail.com>
2026-04-16 22:32:22 -0300

    feat: implement AuthUser JPA entity, profile creation DTO, and profile creation use case

--- backend/xchange_api/src/main/java/br/com/xchange/api/application/dto/request/CreateProfileRequestDto.java
+++ backend/xchange_api/src/main/java/br/com/xchange/api/application/dto/request/CreateProfileRequestDto.java
@@ -5,10 +5,20 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import br.com.xchange.api.domain.entities.Profile;
+
 public record CreateProfileRequestDto(
   @JsonProperty(value = "user_id")
   UUID userId,
 
   @JsonProperty(value = "favorite_cryptos")
   Set<String> favoriteCryptos
-) {}
+) {
+  public Profile toDomain() {
+    Profile profile = new Profile();
+    profile.setId(userId);
+    profile.setFavoriteCryptos(favoriteCryptos);
+
+    return profile;
+  }
+}
