427aefce1bc4fb390ab2959ddfe569257a7b7e34
zielisgabriel <josegabrielas0586@email.com>
2026-04-18 23:38:51 -0300

    delete

--- backend/xchange_api/src/main/java/br/com/xchange/api/application/dto/response/coingecko/Coin.java
+++
@@ -1,5 +0,0 @@
-package br.com.xchange.api.application.dto.response.coingecko;
-
-record Coin(
-  Object item
-) {}
--- backend/xchange_api/src/main/java/br/com/xchange/api/application/dto/response/coingecko/CoinsResponse.java
+++
@@ -1,7 +0,0 @@
-package br.com.xchange.api.application.dto.response.coingecko;
-
-import java.util.List;
-
-public record CoinsResponse(
-  List<ItemCoin> coins
-) {}
