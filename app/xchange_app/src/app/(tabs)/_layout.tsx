6bee43a09af0717b6af7196738c7d7ce8510077e
zielisgabriel <josegabrielas0586@gmail.com>
2026-04-22 22:22:35 -0300

    feat: initialize Expo project structure and Spring Boot backend configuration

--- app/xchange_app/src/app/(tabs)/_layout.tsx
+++ app/xchange_app/src/app/(tabs)/_layout.tsx
@@ -1,10 +1,25 @@
 import { Tabs } from "expo-router";
+import { HomeIcon, UserIcon } from "lucide-react-native";
 
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ headerShown: false }} />
-      <Tabs.Screen name="profile" options={{ headerShown: false }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          title: "Início",
+          tabBarIcon: ({color, size}) => <HomeIcon color={color} size={size} />
+        }} 
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          title: "Perfil",
+          tabBarIcon: ({color, size}) => <UserIcon color={color} size={size} />
+        }}
+      />
     </Tabs>
   )
 }
--- app/xchange_app/src/app/(tabs)/index.tsx
+++ app/xchange_app/src/app/(tabs)/index.tsx
@@ -1,13 +1,16 @@
 import { Text } from "@/components/ui/text";
 import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function Index() {
   return (
-    <View>
-      <Text className="text-foreground">
-        Home screen
-      </Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text className="text-foreground">
+          Home screen
+        </Text>
+      </View>
+    </SafeAreaView>
   )
 }
