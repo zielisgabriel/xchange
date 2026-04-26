32cb3b340f6d6c460d8c81ee8c427e93beee29bc
zielisgabriel <josegabrielas0586@gmail.com>
2026-04-26 06:54:09 -0300

    feat: implement CoinGecko integration for trending coins with Redis caching

--- backend/xchange_api/src/main/java/br/com/xchange/api/application/usecase/GetTrendingCoinUseCase.java
+++ backend/xchange_api/src/main/java/br/com/xchange/api/application/usecase/GetTrendingCoinUseCase.java
@@ -1,8 +1,33 @@
 package br.com.xchange.api.application.usecase;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-@Service
-public class GetTrendingCoinUseCase {
+import br.com.xchange.api.application.dto.response.TrendingCoinResponse;
+import br.com.xchange.api.application.dto.response.TrendingCoinResponse.TrendingCoinWrapper;
+import br.com.xchange.api.domain.ports.services.CoinServicePort;
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
+public class GetTrendingCoinUseCase {
+  private final CoinServicePort coinServicePort;
+
+  public TrendingCoinResponse execute() {
+    List<TrendingCoinWrapper> coins = this.coinServicePort.getTrendingCoinsDetailed().stream()
+      .map(data -> new TrendingCoinResponse.TrendingCoinWrapper(
+        data.coin().getId(),
+        data.coin().getName(),
+        data.coin().getSymbol(),
+        data.coin().getImageUrl(),
+        data.coin().getPrice(),
+        data.priceBtc(),
+        data.marketCap(),
+        data.totalVolume(),
+        data.sparkline()
+      )).toList();
+
+    return new TrendingCoinResponse(coins);
+  }
 }
