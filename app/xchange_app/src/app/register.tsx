707b77e9092fa9f9a67da770c428f4f1316f6dc3
zielisgabriel <josegabrielas0586@email.com>
2026-05-06 00:24:20 -0300

    feat(register): create register screen

---
+++ app/xchange_app/src/app/register.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { View } from "react-native";
+import GoogleIcon from "@/assets/icons8-google.svg"
+import { Separator } from "@/components/ui/separator";
+import { Link } from "expo-router";
+import { LoginForm } from "@/components/login-form";
+import { RegisterForm } from "@/components/register-form";
+
+export default function Register() {
+  return (
+    <View className="flex justify-center h-full w-full px-4">
+      <View className="mb-4">
+        <Text className="text-4xl" variant={"large"}>
+          Cadastrar
+        </Text>
+        <Text className="text-lg" variant={"muted"}>
+          uma conta
+        </Text>
+      </View>
+
+      <RegisterForm />
+
+      <View>
+        <Separator className="my-4" />
+      </View>
+
+      {/* <View>
+        <Button variant={"outline"} className="text-foreground">
+          <GoogleIcon width={32} height={32} />
+          <Text>
+            Entrar com uma conta
+          </Text>
+        </Button>
+      </View> */}
+
+      <View className="flex flex-row justify-center gap-1 mt-4">
+        <Text className="text-sm">
+          Já tem uma conta?
+        </Text>
+        <Link href={"/register"}>
+          <Text className="text-sm font-bold text-blue-400 underline">
+            Entrar
+          </Text>
+        </Link>
+      </View>
+    </View>
+  )
+}
