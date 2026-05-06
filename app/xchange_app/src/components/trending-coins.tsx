80ece0c28c356363f8d285d67e8808cdf5264786
zielisgabriel <josegabrielas0586@email.com>
2026-05-06 00:25:17 -0300

    refactor(coins): add "show more"

--- app/xchange_app/src/components/trending-coins.tsx
+++ app/xchange_app/src/components/trending-coins.tsx
@@ -7,12 +7,18 @@ import { ScrollView } from "react-native-gesture-handler"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import { Separator } from "./ui/separator"
 import { Image } from "expo-image"
+import { useState } from "react"
+import { ChevronDown, ChevronUp } from "lucide-react-native"
+import clsx from "clsx"
+import { twMerge } from "tailwind-merge"
 
 interface TrendingCoinResponse {
   coins: CoinWithMarketData[]
 }
 
 export function TrendingCoins() {
+  const [showMoreCoins, setShowMoreCoins] = useState<boolean>(false);
+
   async function getTrendingCoins() {
     const response = await fetch("/api/coins/trending", {
       method: "GET",
@@ -39,7 +45,7 @@ export function TrendingCoins() {
   return (
     <ScrollView>
         <View className="flex">
-          {trendingCoins?.coins.map(coin => (
+          {trendingCoins?.coins.slice(0, showMoreCoins ? 15 : 5).map((coin, index) => (
             <View key={coin.id}>
               <Button
                 className="text-white h-16 py-0 px-4 justify-between rounded-none"
@@ -48,6 +54,9 @@ export function TrendingCoins() {
               >
                 <View>
                   <View className="flex flex-row gap-2 items-center">
+                    <Text className="text-muted-foreground">
+                      # {index + 1}
+                    </Text>
                     <Avatar alt={coin.name}>
                       <AvatarImage source={{ uri: coin.imageUrl }} />
                     </Avatar>
@@ -69,11 +78,23 @@ export function TrendingCoins() {
                     {currencyFormat.format(coin.price)}
                   </Text>
                 </View>
-
               </Button>
               <Separator />
             </View>
           ))}
+
+          <Button variant={"link"} onPress={() => setShowMoreCoins(!showMoreCoins)}>
+            <View>
+              <Text className="flex flex-row items-center gap-1 text-muted-foreground">
+                {showMoreCoins ? (
+                  <ChevronUp />
+                ) : (
+                  <ChevronDown />
+                )}
+                Mostrar {showMoreCoins ? "menos" : "mais"}
+              </Text>
+            </View>
+          </Button>
       </View>
     </ScrollView>
   )
